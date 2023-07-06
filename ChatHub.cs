e2ffb896bee87b86d96e74381a469b393036cea6
sabegomaa261 <sabergomaa261@gmail.com>
2023-07-06 14:07:02 +0300

    Add project files.

---
+++ ChatHub.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNet.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SignalR
+{
+    public class ChatHub : Hub
+    {
+        public void sendMessage(string name ,string message)
+        {
+            Clients.All.newMessage(name, message);
+        }
+    }
+}
